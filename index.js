787691992ce76db0ad48be82ce88eaf65bff1bcf
rautjs2002 <rautjs2002@gmail.com>
2023-03-28 05:03:48 -0700

    js added

---
+++ index.js
@@ -0,0 +1,16 @@
+let movie = [
+    {
+        image : "images/big-img.png"
+    },
+    {
+        image : "images/big-img2.png"
+    },
+    {
+        images :"/big-img3.png"
+    }
+];
+
+const caraousel=document.querySelector('.caraousel');
+let sliders=[];
+
+let sliderIndex=0;
